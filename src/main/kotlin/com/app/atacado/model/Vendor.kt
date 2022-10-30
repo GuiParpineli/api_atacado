03bdbbdb732c182b6b4df2d469b0503d082b62bc
Gui Parpineli <guiparpineli@GuiPC>
2022-10-30 16:07:36 -0300

    add user in vvendor service

--- src/main/kotlin/com/app/atacado/model/Vendor.kt
+++ src/main/kotlin/com/app/atacado/model/Vendor.kt
@@ -8,7 +8,9 @@ data class Vendor(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     var name: String = "",
+    var lastname: String = "",
     @Column(nullable = false, unique = true)
     var cpf: String = "",
-    var comission: Int = 10
+    var comission: Int = 10,
+    var password: String = ""
 )
--- src/main/kotlin/com/app/atacado/service/VendorService.kt
+++ src/main/kotlin/com/app/atacado/service/VendorService.kt
@@ -1,12 +1,15 @@
 package com.app.atacado.service
 
+import com.app.atacado.model.SystemUser
+import com.app.atacado.model.SystemUserRoles
 import com.app.atacado.model.Vendor
+import com.app.atacado.repository.SystemUserRepository
 import com.app.atacado.repository.VendorRepository
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class VendorService(val repository: VendorRepository) {
+class VendorService(val repository: VendorRepository, val userRepository: SystemUserRepository) {
 
     fun getAll(): List<Vendor> {
         return repository.findAll()
@@ -16,8 +19,18 @@ class VendorService(val repository: VendorRepository) {
         return repository.findById(id)
     }
 
-    fun save(Vendor: Vendor): Vendor {
-        return repository.save(Vendor)
+    fun save(vendor: Vendor): Vendor {
+        userRepository.save(
+            SystemUser(
+                null,
+                vendor.name,
+                vendor.lastname,
+                vendor.cpf,
+                vendor.password,
+                SystemUserRoles.ROLE_VENDOR
+            )
+        )
+        return repository.save(vendor)
     }
 
     fun update(Vendor: Vendor): Vendor {
